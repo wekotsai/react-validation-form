914ec6b12c8169b183da90cdc484d0a268698689
Wéko <42733086+wekotsai@users.noreply.github.com>
2021-12-17 17:43:31 +0100

    :art: add more conditions and error messages

--- src/App.js
+++ src/App.js
@@ -5,28 +5,55 @@ export default function App() {
   const [isValid, setIsValid] = useState(false);
   const [message, setMessage] = useState('');
 
-  const emailRegex = /\S+@\S+\.\S+/;
-  const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  const emailRules = /\S+@\S+\.\S+/;
+  const passwordRules = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
   const validateEmail = (event) => {
     const email = event.target.value;
-    if (emailRegex.test(email)) {
+    if (emailRules.test(email)) {
       setIsValid(true);
-      setMessage('Your email looks good!');
+      setMessage('✅ Your email looks good!');
     } else {
       setIsValid(false);
-      setMessage('Please enter a valid email!');
+      setMessage('❗Please enter a valid email!');
     }
   };
 
   const validatePassword = (event) => {
     const password = event.target.value;
-    if (passwordRegex.test(password)) {
-      setIsValid(true);
-      setMessage('Your password looks good!');
-    } else {
-      setIsValid(false);
-      setMessage('Please enter a valid password!');
+      if (passwordRules.test(password)) {
+        setIsValid(true);
+        setMessage('✅ Your password looks good!');
+      }
+
+      if (password.length < 8) { {
+        setIsValid(false);
+        setMessage('❗At least 8 characters');
+      }
+
+      if (password.search(/[a-z]/) < 0) {
+        setIsValid(false);
+        setMessage('❗At least one lowercase letter');
+      }
+
+      if (password.search(/[A-Z]/) < 0) {
+        setIsValid(false);
+        setMessage('❗At least one uppercase letter');
+      }
+
+      if (password.search(/[0-9]/) < 0) {
+        setIsValid(false);
+        setMessage('❗At least one number');
+      }
+
+      if (password.search(/[$@#&!]/) < 0) {
+        setIsValid(false);
+        setMessage('❗At least one special character');
+      }
+
+      if (password.length === 0) {
+        setMessage('');
+      }
     }
   };
 
@@ -42,8 +69,6 @@ export default function App() {
           name="email"
           onChange={validateEmail}
         />
-        <br />
-        <br />
         <label>Password:</label>
         <input
           type="text"
@@ -51,8 +76,6 @@ export default function App() {
           name="password"
           onChange={validatePassword}
         />
-        <br />
-        <br />
         <button type="submit">Submit</button>
         <div className={`message ${isValid ? 'success' : 'error'}`}>
           {message}
