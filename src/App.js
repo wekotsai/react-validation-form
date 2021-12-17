d5ba358fc7dd2ac0c6eef1650910074ff6c76366
Wéko <42733086+wekotsai@users.noreply.github.com>
2021-12-17 16:33:11 +0100

    :art: add validation rules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [isValid, setIsValid] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const emailRegex = /\S+@\S+\.\S+/;
+  const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+  const validateEmail = (event) => {
+    const email = event.target.value;
+    if (emailRegex.test(email)) {
+      setIsValid(true);
+      setMessage('Your email looks good!');
+    } else {
+      setIsValid(false);
+      setMessage('Please enter a valid email!');
+    }
+  };
+
+  const validatePassword = (event) => {
+    const password = event.target.value;
+    if (passwordRegex.test(password)) {
+      setIsValid(true);
+      setMessage('Your password looks good!');
+    } else {
+      setIsValid(false);
+      setMessage('Please enter a valid password!');
+    }
+  };
+
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Join us today!</h1>
       <form>
-        <label>
-          Email:
-          <input type="text" name="email" />
-        </label>
-        <br></br>
-        <label>
-          Password:
-          <input type="text" name="Password" />
-        </label>
-        <br></br>
-        <input type="submit" value="Submit" />
+        <label>Email:</label>
+        <input
+          type="email"
+          required
+          name="email"
+          onChange={validateEmail}
+        />
+        <br />
+        <br />
+        <label>Password:</label>
+        <input
+          type="text"
+          required
+          name="password"
+          onChange={validatePassword}
+        />
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+        <div className={`message ${isValid ? 'success' : 'error'}`}>
+          {message}
+        </div>
       </form>
     </div>
   );
 }
-
-export default App;
